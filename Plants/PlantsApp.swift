cbccdd055c32db0746ee6ac98c01532e3b2c8f9f
Tahani-mohammed <Tahaniii.moh@gmail.com>
2024-10-27 16:03:21 +0300

    Initial Commit

---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by Tahani Mohammed on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
